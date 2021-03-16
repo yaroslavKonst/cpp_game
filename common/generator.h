6c767aa7f6c72f2280c65bc0ac82cf529810af88
EvgenyEmets <e-evgeny99@ya.ru>
2021-03-16 21:47:38 +0300

    Add Chunk generator

--- common/generator.h
+++ common/generator.h
@@ -5,7 +5,7 @@
 #include <random>
 #include <vector>
 
-void generator (uint64_t seed, int32_t x, int32_t y) {
+Chunk* generator (uint64_t seed, int32_t x, int32_t y) {
     uint64_t ChunkSeed = seed;
     ChunkSeed = ChunkSeed ^ (int64_t(x) << 32);
     ChunkSeed = ChunkSeed ^ (int64_t(y) & 0xFFFFFFFF);
@@ -17,6 +17,7 @@ void generator (uint64_t seed, int32_t x, int32_t y) {
     }
 
     std::vector<int> Postgen(CHUNKSIZE * CUNKSIZE);
+    Layer L;
     for (int i = 0; i < CHUNKSIZE * CUNKSIZE; i++) {
         int lu = i - CHUNKSIZE - 1, mu = i - CHUNKSIZE, ru = i - CHUNKSIZE + 1, 
                 rm = i + 1, rd = i + CHUNKSIZE + 1, md = i + CHUNKSIZE, 
@@ -41,8 +42,16 @@ void generator (uint64_t seed, int32_t x, int32_t y) {
         }
         Postgen[i] = Predgen[lu] + Predgen[mu] + Predgen[ru] + Predgen[rm] + 
                 Predgen[rd] + Predgen[md] + Predgen[ld] + Predgen[lm];
+        if (Postgen[i] > 3 && Postgen[i] < 13) {
+            L.SetTile(i % CHUNKSIZE, i / CHUNKSIZE, Tile::Grass);
+        } else {
+            L.SetTile(i % CHUNKSIZE, i / CHUNKSIZE, Tile::Stone);
+        }
     }
-    
+    Chunk C;
+    C.AddLayer(&L);
+    return &C;
+}
 
 
 
