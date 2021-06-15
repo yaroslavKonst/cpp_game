c254877f4e1268c5b95da3fa44a8c021d8d98b8f
Кузьмин Ярослав <s02170137@practicum.cs.msu.su>
2021-06-15 17:31:42 +0300

    Add swapchain to the rendering module.

--- src/client/video.cpp
+++ src/client/video.cpp
@@ -2,9 +2,12 @@
 #include <cstring>
 #include "video.h"
 
-Video::Video(int width, int height, bool validate):
+Video::Video(int w, int h, bool validate):
 	validationLayers(0)
 {
+	width = w;
+	height = h;
+
 	enableValidationLayers = validate;
 	if (validate) {
 		validationLayers.push_back(
@@ -15,7 +18,9 @@ Video::Video(int width, int height, bool validate):
 			"VK_LAYER_LUNARG_standard_validation");
 	}
 
-	InitWindow(width, height);
+	deviceExtensions.push_back(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
+
+	InitWindow();
 	InitVulkan();
 }
 
@@ -25,7 +30,7 @@ Video::~Video()
 	CloseWindow();
 }
 
-void Video::InitWindow(int width, int height)
+void Video::InitWindow()
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -91,6 +96,7 @@ void Video::CreateInstance()
 
 void Video::CloseVulkan()
 {
+	vkDestroySwapchainKHR(device, swapchain, nullptr);
 	vkDestroyDevice(device, nullptr);
 	vkDestroySurfaceKHR(instance, surface, nullptr);
 	vkDestroyInstance(instance, nullptr);
@@ -134,24 +140,38 @@ void Video::CreateLogicalDevice()
 {
 	QueueFamilyIndices indices = FindQueueFamilies(physicalDevice);
 
-	VkDeviceQueueCreateInfo queueCreateInfo{};
-	queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-	queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
-	queueCreateInfo.queueCount = 1;
+	std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+	std::set<uint32_t> uniqueQueueFamilies = {
+		indices.graphicsFamily.value(),
+		indices.presentFamily.value()
+	};
 
 	float queuePriority = 1.0f;
-	queueCreateInfo.pQueuePriorities = &queuePriority;
+
+	for (uint32_t queueFamily : uniqueQueueFamilies) {
+		VkDeviceQueueCreateInfo queueCreateInfo{};
+		queueCreateInfo.sType =
+			VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queueCreateInfo.queueFamilyIndex = queueFamily;
+		queueCreateInfo.queueCount = 1;
+		queueCreateInfo.pQueuePriorities = &queuePriority;
+
+		queueCreateInfos.push_back(queueCreateInfo);
+	}
 
 	VkPhysicalDeviceFeatures deviceFeatures{};
 
 	VkDeviceCreateInfo createInfo{};
 	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-	createInfo.queueCreateInfoCount = 1;
-	createInfo.pQueueCreateInfos = &queueCreateInfo;
+	createInfo.queueCreateInfoCount =
+		static_cast<uint32_t>(queueCreateInfos.size());
+	createInfo.pQueueCreateInfos = queueCreateInfos.data();
 
 	createInfo.pEnabledFeatures = &deviceFeatures;
 
-	createInfo.enabledExtensionCount = 0;
+	createInfo.enabledExtensionCount =
+		static_cast<uint32_t>(deviceExtensions.size());
+	createInfo.ppEnabledExtensionNames = deviceExtensions.data();
 
 	if (enableValidationLayers) {
 		createInfo.enabledLayerCount = static_cast<uint32_t>(
@@ -172,6 +192,132 @@ void Video::CreateLogicalDevice()
 		indices.graphicsFamily.value(),
 		0,
 		&graphicsQueue);
+
+	vkGetDeviceQueue(
+		device,
+		indices.presentFamily.value(),
+		0,
+		&presentQueue);
+}
+
+void Video::CreateSwapchain()
+{
+	SwapchainSupportDetails swapchainSupport =
+		QuerySwapchainSupport(physicalDevice);
+
+	VkSurfaceFormatKHR surfaceFormat =
+		ChooseSwapSurfaceFormat(swapchainSupport.formats);
+	VkPresentModeKHR presentMode =
+		ChooseSwapPresentMode(swapchainSupport.presentModes);
+	VkExtent2D extent = ChooseSwapExtent(swapchainSupport.capabilities);
+
+	uint32_t imageCount = swapchainSupport.capabilities.minImageCount + 2;
+
+	if (swapchainSupport.capabilities.maxImageCount > 0 &&
+		imageCount > swapchainSupport.capabilities.maxImageCount)
+	{
+		imageCount = swapchainSupport.capabilities.maxImageCount;
+	}
+
+	VkSwapchainCreateInfoKHR createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	createInfo.surface = surface;
+
+	createInfo.minImageCount = imageCount;
+	createInfo.imageFormat = surfaceFormat.format;
+	createInfo.imageColorSpace = surfaceFormat.colorSpace;
+	createInfo.imageExtent = extent;
+	createInfo.imageArrayLayers = 1;
+	createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+	QueueFamilyIndices indices = FindQueueFamilies(physicalDevice);
+
+	uint32_t queueFamilyIndices[] = {
+		indices.graphicsFamily.value(),
+		indices.presentFamily.value()\
+	};
+
+	if (indices.graphicsFamily.value() != indices.presentFamily.value()) {
+		createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+		createInfo.queueFamilyIndexCount = 2;
+		createInfo.pQueueFamilyIndices = queueFamilyIndices;
+	} else {
+		createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		createInfo.queueFamilyIndexCount = 0;
+		createInfo.pQueueFamilyIndices = nullptr;
+	}
+
+	createInfo.preTransform =
+		swapchainSupport.capabilities.currentTransform;
+
+	createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+
+	createInfo.presentMode = presentMode;
+	createInfo.clipped = VK_TRUE;
+
+	createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+	if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapchain) !=
+		VK_SUCCESS)
+	{
+		throw std::runtime_error("failed to create swapchain");
+	}
+
+	vkGetSwapchainImagesKHR(device, swapchain, &imageCount, nullptr);
+	swapchainImages.resize(imageCount);
+	vkGetSwapchainImagesKHR(
+		device,
+		swapchain,
+		&imageCount,
+		swapchainImages.data());
+
+	swapchainImageFormat = surfaceFormat.format;
+	swapchainExtent = extent;
+}
+
+Video::SwapchainSupportDetails Video::QuerySwapchainSupport(
+	VkPhysicalDevice device)
+{
+	SwapchainSupportDetails details;
+
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(
+		device,
+		surface,
+		&details.capabilities);
+
+	uint32_t formatCount;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(
+		device,
+		surface,
+		&formatCount,
+		nullptr);
+
+	if (formatCount > 0) {
+		details.formats.resize(formatCount);
+		vkGetPhysicalDeviceSurfaceFormatsKHR(
+			device,
+			surface,
+			&formatCount,
+			details.formats.data());
+	}
+
+	uint32_t presentModeCount;
+	vkGetPhysicalDeviceSurfacePresentModesKHR(
+		device,
+		surface,
+		&presentModeCount,
+		nullptr);
+
+	if (presentModeCount > 0) {
+		details.presentModes.resize(presentModeCount);
+		vkGetPhysicalDeviceSurfacePresentModesKHR(
+			device,
+			surface,
+			&presentModeCount,
+			details.presentModes.data());
+	}
+
+	return details;
 }
 
 bool Video::IsDeviceSuitable(VkPhysicalDevice device) {
@@ -180,10 +326,106 @@ bool Video::IsDeviceSuitable(VkPhysicalDevice device) {
 	vkGetPhysicalDeviceProperties(device, &deviceProperties);
 	vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
 
+	bool extensionsSupported = CheckDeviceExtensionSupport(device);
+
+	bool swapchainAdequate = false;
+	if (extensionsSupported) {
+		SwapchainSupportDetails swapchainSupport =
+			QuerySwapchainSupport(device);
+		swapchainAdequate = !swapchainSupport.formats.empty() &&
+			!swapchainSupport.presentModes.empty();
+	}
+
 	return deviceProperties.deviceType ==
 		VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
-           deviceFeatures.geometryShader &&
-	   FindQueueFamilies(device).graphicsFamily.has_value();
+		extensionsSupported &&
+		swapchainAdequate &&
+		deviceFeatures.geometryShader &&
+		FindQueueFamilies(device).graphicsFamily.has_value() &&
+		FindQueueFamilies(device).presentFamily.has_value();
+}
+
+VkSurfaceFormatKHR Video::ChooseSwapSurfaceFormat(
+	const std::vector<VkSurfaceFormatKHR>& availableFormats)
+{
+	for (const auto& availableFormat : availableFormats) {
+		if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB &&
+			availableFormat.colorSpace ==
+			VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+		{
+			return availableFormat;
+		}
+	}
+
+	return availableFormats[0];
+}
+
+VkPresentModeKHR Video::ChooseSwapPresentMode(
+	const std::vector<VkPresentModeKHR>& availablePresentModes)
+{
+	for (const auto& availablePresentMode : availablePresentModes) {
+		if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+			return availablePresentMode;
+		}
+	}
+
+	return VK_PRESENT_MODE_FIFO_KHR;
+}
+
+VkExtent2D Video::ChooseSwapExtent(
+	const VkSurfaceCapabilitiesKHR& capabilities)
+{
+	if (capabilities.currentExtent.width != UINT32_MAX) {
+		return capabilities.currentExtent;
+	} else {
+		int width;
+		int height;
+
+		glfwGetFramebufferSize(window, &width, &height);
+
+		VkExtent2D actualExtent = {
+			static_cast<uint32_t>(width),
+			static_cast<uint32_t>(height)
+		};
+
+		actualExtent.width = std::clamp(
+			actualExtent.width,
+			capabilities.minImageExtent.width,
+			capabilities.maxImageExtent.width);
+		actualExtent.height = std::clamp(
+			actualExtent.height,
+			capabilities.minImageExtent.height,
+			capabilities.maxImageExtent.height);
+
+		return actualExtent;
+	}
+}
+
+bool Video::CheckDeviceExtensionSupport(VkPhysicalDevice device)
+{
+	uint32_t extensionCount;
+	vkEnumerateDeviceExtensionProperties(
+		device,
+		nullptr,
+		&extensionCount,
+		nullptr);
+
+	std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+	vkEnumerateDeviceExtensionProperties(
+		device,
+		nullptr,
+		&extensionCount,
+		availableExtensions.data());
+
+	std::set<std::string> requiredExtensions(
+		deviceExtensions.begin(),
+		deviceExtensions.end());
+
+	for (const auto& extension : availableExtensions) {
+		requiredExtensions.erase(extension.extensionName);
+	}
+
+	return requiredExtensions.empty();
 }
 
 Video::QueueFamilyIndices Video::FindQueueFamilies(VkPhysicalDevice device)
@@ -207,6 +449,17 @@ Video::QueueFamilyIndices Video::FindQueueFamilies(VkPhysicalDevice device)
 	for (const auto& queueFamily : queueFamilies) {
 		if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
 			indices.graphicsFamily = i;
+		}
+
+		VkBool32 presentSupport = false;
+		vkGetPhysicalDeviceSurfaceSupportKHR(
+			device,
+			i,
+			surface,
+			&presentSupport);
+
+		if (presentSupport) {
+			indices.presentFamily = i;
 		}
 
 		i++;
--- src/client/video.h
+++ src/client/video.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 #include <optional>
+#include <set>
+#include <algorithm>
 
 #include <vulkan/vulkan.h>
 #define GLFW_INCLUDE_VULKAN
@@ -16,33 +18,62 @@ class Video
 	struct QueueFamilyIndices
 	{
 		std::optional<uint32_t> graphicsFamily;
+		std::optional<uint32_t> presentFamily;
+	};
+
+	struct SwapchainSupportDetails
+	{
+		VkSurfaceCapabilitiesKHR capabilities;
+		std::vector<VkSurfaceFormatKHR> formats;
+		std::vector<VkPresentModeKHR> presentModes;
 	};
 
 	VkInstance instance;
 	VkPhysicalDevice physicalDevice;
 	VkDevice device;
 	VkQueue graphicsQueue;
+	VkQueue presentQueue;
 	GLFWwindow* window;
 	VkSurfaceKHR surface;
+	VkSwapchainKHR swapchain;
+	std::vector<VkImage> swapchainImages;
+	VkFormat swapchainImageFormat;
+	VkExtent2D swapchainExtent;
+
+	uint32_t width;
+	uint32_t height;
 
 	bool enableValidationLayers;
 	std::vector<const char*> validationLayers;
 
-	void InitWindow(int width, int height);
+	std::vector<const char*> deviceExtensions;
+
+	void InitWindow();
 	void CloseWindow();
+
 	void InitVulkan();
 	void CloseVulkan();
 	void CreateInstance();
 	void CreateSurface();
 	void PickPhysicalDevice();
 	void CreateLogicalDevice();
+	void CreateSwapchain();
 	bool IsDeviceSuitable(VkPhysicalDevice device);
 	QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
 	bool CheckValidationLayerSupport(
 		std::vector<const char*> validationLayers);
+	bool CheckDeviceExtensionSupport(VkPhysicalDevice device);
+	SwapchainSupportDetails QuerySwapchainSupport(VkPhysicalDevice device);
+	VkSurfaceFormatKHR ChooseSwapSurfaceFormat(
+		const std::vector<VkSurfaceFormatKHR>& availableFormats);
+	VkPresentModeKHR ChooseSwapPresentMode(
+		const std::vector<VkPresentModeKHR>& availablePresentModes);
+	VkExtent2D ChooseSwapExtent(
+		const VkSurfaceCapabilitiesKHR& capabilities);
+
 	void MainLoop();
 public:
-	Video(int width, int height, bool validate = false);
+	Video(int w, int h, bool validate = false);
 	~Video();
 };
 
